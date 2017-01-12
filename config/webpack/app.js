b8bab0ca01abe85131d3dd758c167b503a24ed71
tongchia <tongchia@live.com>
2017-01-12 15:02:01 +0800

    Merge webpack config;

--- config/webpack/app.js
+++ config/webpack/app.js
@@ -3,32 +3,46 @@ const webpack = require('webpack');
 const cssnext = require('postcss-cssnext');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const base = require('./base');
-const config = require('../development');
 const isDll = !!process.env.DLL_FILES;
+const env = process.env.NODE_EMV || 'development';
+const config = require(`../${env}.js`);
 const dllFiles = isDll ? process.env.DLL_FILES.split(',') : [];
+const isDev = env === 'development';
+const isDist = env === 'production';
+const isTest = env === 'test';
 
-const cache = true;
-const watch = true;
-const devtool = '#source-map';
+const cache = isDev;
+const watch = isDev;
+const devtool = isDev ? '#source-map' : isTest ? 'eval' : 'source-map';
 const plugins = [
   new webpack.optimize.OccurrenceOrderPlugin(),
   // new webpack.WatchIgnorePlugin('.tmp/'),
   // new webpack.NoErrorsPlugin(),
   new webpack.LoaderOptionsPlugin({
-    minimize: false,
-    debug: true,
+    minimize: isDist,
+    debug: isDev,
     options: {
       postcss: () => [cssnext]
-    },
+    }
   }),
   new HtmlWebpackPlugin({
     title: config.appName,
     lang: config.lang,
     dll: dllFiles,
+    hash: isDist,
+    minify: isDist || {
+      removeComments: true,
+      collapseWhitespace: true
+    },
     template: `${config.src}/index.html`,
     filename: 'index.html',
   })
 ];
+
+if (isDist) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+  plugins.push(new webpack.optimize.AggressiveMergingPlugin());
+}
 
 if (isDll) {
   dllFiles.forEach(dll => {
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,4 @@
-const dev = require('./config/webpack/dev');
-const dist = require('./config/webpack/dist');
-const test = require('./config/webpack/test');
-
 /* eslint-env node */
-const env = process.env.NODE_ENV;
+const app = require('./config/webpack/app');
 
-module.exports = env == 'production' ? dist : env == 'test' ? test : dev;
+module.exports = app;
